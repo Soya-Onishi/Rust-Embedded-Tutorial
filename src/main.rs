bbb753b4af2d432ed1bdef29079427bc8704c600
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-11-01 21:32:02 +0900

    tm4c123xマイクロコントローラ向けコードに変更

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,25 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
 use core::fmt::Write;
+
 use cortex_m_rt::entry;
 use cortex_m_semihosting::{debug, hio};
+use cortex_m::peripheral::{syst, Peripherals};
 
 #[entry]
 fn main() -> ! {
-    let mut stdout = hio::hstdout().unwrap();    
-    writeln!(stdout, "hello world").unwrap();
+    let peripherals = Peripherals::take().unwrap();
+    let mut systick = peripherals.SYST;
+    systick.set_clock_source(syst::SystClkSource::Core);
+    systick.set_reload(1_000);
+    systick.clear_current();
+    systick.enable_counter();
 
+    while !systick.has_wrapped() {}
+
+    let mut stdout = hio::hstdout().unwrap();
+    writeln!(stdout, "clock is done").unwrap();
     debug::exit(debug::EXIT_SUCCESS);
 
-    loop {
-        
-    }
+    loop {}
 }
