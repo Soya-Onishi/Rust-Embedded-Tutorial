47fb3a80bf0ec7b35352cd2a7ba7c11a6c844c54
SoyaOhnishi <soya.ohnishi@gmail.com>
2022-11-02 23:13:17 +0900

    volatile_registerクレートを用いた処理に変更

--- src/main.rs
+++ src/main.rs
@@ -11,26 +11,51 @@ use core::fmt::Write;
 
 use cortex_m::peripheral::syst::SystClkSource;
 use cortex_m_rt::{entry, exception};
+use volatile_register::{RW, RO};
 use cortex_m_semihosting::{
     debug,
     hio::{self, HStdout}
 };
 
-#[repr(C)]
 struct SysTick {
-    pub csr: u32,
-    pub rvr: u32,
-    pub cvr: u32,
-    pub calib: u32
+    p: &'static mut RegisterBlock
+}
+
+#[repr(C)]
+struct RegisterBlock {
+    pub csr: RW<u32>,
+    pub rvr: RW<u32>,
+    pub cvr: RW<u32>,
+    pub calib: RO<u32>
+}
+
+impl SysTick {
+    pub fn new() -> SysTick {
+        SysTick {
+            p: unsafe { &mut *(0xE000_E010 as *mut RegisterBlock) }
+        }
+    }
+
+    pub fn get_time(&self) -> u32 {
+        self.p.cvr.read()
+    }
+
+    pub fn set_reload(&mut self, reload_value: u32) {
+        unsafe { self.p.rvr.write(reload_value); }
+    }
 }
 
 #[entry]
 fn main() -> ! {
-    let systick = unsafe { &mut *(0xE000_E010 as *mut SysTick) };
-    let time =  unsafe { core::ptr::read_volatile(&mut systick.cvr)};
     let mut stdout = hio::hstdout().unwrap();
+    let mut syst = SysTick::new();
+    syst.set_reload(0x0012_0000);
 
-    writeln!(stdout, "time: {}", time).unwrap();
+    for _ in 0..100 {
+        writeln!(stdout, "time: {}", syst.get_time()).unwrap();
+    }
+
+    debug::exit(debug::EXIT_SUCCESS);
 
     loop {}
 }
